0d2880bab0362c78e54e6be6f031d18966dcf6e6
Erica <hi.ericajung@gmail.com>
2017-01-17 17:35:59 -0800

    Used CSS code following BEM principle

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 
@@ -21,7 +22,7 @@ gulp.task('html', function(){
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
 		//2. adding autoprefixer
-		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 		//1. having a one pipe gulp.src to gulp.dest
 		.pipe(gulp.dest('./app/temp/styles'));
 
